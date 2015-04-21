ce928cd35af3f21eebd751ebd0f1144d301202a3
Toaker <ToakerQin@gmail.com>
2015-04-21 21:31:13 +0800

    Update TLog.java！
    Signed-off-by: Toaker <ToakerQin@gmail.com>

--- tlog/src/main/java/com/toaker/common/tlog/TLog.java
+++ tlog/src/main/java/com/toaker/common/tlog/TLog.java
@@ -39,465 +39,324 @@ public final class TLog {
 
     public static final int ASSERT = Log.ASSERT;
 
-    private static LoggingDelegate sHandler = TLogDefaultLoggingDelegate.getInstance();
-
-    /**
-     * Sets the logging delegate that overrides the default delegate.
-     *
-     * @param delegate the delegate to use
-     */
-    public static void setLoggingDelegate(LoggingDelegate delegate) {
-        if (delegate == null) {
-            throw new IllegalArgumentException();
-        }
-        sHandler = delegate;
-    }
-
-    public static boolean isLoggable(int level) {
-        return sHandler.isLoggable(level);
-    }
-
-    public static void setMinimumLoggingLevel(int level) {
-        sHandler.setMinimumLoggingLevel(level);
-    }
-
-    public static int getMinimumLoggingLevel() {
-        return sHandler.getMinimumLoggingLevel();
-    }
 
     public static void v(String tag, String msg) {
-        if (sHandler.isLoggable(VERBOSE)) {
-            sHandler.v(tag, msg);
-        }
+        Log.v(tag, msg);
     }
 
     public static void v(String tag, String msg, Object arg1) {
-        if (sHandler.isLoggable(VERBOSE)) {
-            sHandler.v(tag, formatString(msg, arg1));
-        }
+        Log.v(tag, formatString(msg, arg1));
     }
 
     public static void v(String tag, String msg, Object arg1, Object arg2) {
-        if (sHandler.isLoggable(VERBOSE)) {
-            sHandler.v(tag, formatString(msg, arg1, arg2));
-        }
+        Log.v(tag, formatString(msg, arg1, arg2));
     }
 
     public static void v(String tag, String msg, Object arg1, Object arg2, Object arg3) {
-        if (sHandler.isLoggable(VERBOSE)) {
-            sHandler.v(tag, formatString(msg, arg1, arg2, arg3));
-        }
+        Log.v(tag, formatString(msg, arg1, arg2, arg3));
     }
 
     public static void v(String tag, String msg, Object arg1, Object arg2, Object arg3, Object arg4) {
-        if (sHandler.isLoggable(VERBOSE)) {
-            sHandler.v(tag, formatString(msg, arg1, arg2, arg3, arg4));
-        }
+        Log.v(tag, formatString(msg, arg1, arg2, arg3, arg4));
     }
 
     public static void v(Class<?> cls, String msg) {
-        if (sHandler.isLoggable(VERBOSE)) {
-            sHandler.v(getTag(cls), msg);
-        }
+        Log.v(getTag(cls), msg);
     }
 
     public static void v(Class<?> cls, String msg, Object arg1) {
-        if (sHandler.isLoggable(VERBOSE)) {
-            sHandler.v(getTag(cls), formatString(msg, arg1));
-        }
+        Log.v(getTag(cls), formatString(msg, arg1));
     }
 
     public static void v(Class<?> cls, String msg, Object arg1, Object arg2) {
-        if (sHandler.isLoggable(VERBOSE)) {
-            sHandler.v(getTag(cls), formatString(msg, arg1, arg2));
-        }
+        Log.v(getTag(cls), formatString(msg, arg1, arg2));
     }
 
     public static void v(Class<?> cls, String msg, Object arg1, Object arg2, Object arg3) {
-        if (isLoggable(VERBOSE)) {
-            v(cls, formatString(msg, arg1, arg2, arg3));
-        }
+        v(cls, formatString(msg, arg1, arg2, arg3));
     }
 
     public static void v(Class<?> cls, String msg, Object arg1, Object arg2, Object arg3,
                          Object arg4) {
-        if (sHandler.isLoggable(VERBOSE)) {
-            sHandler.v(getTag(cls), formatString(msg, arg1, arg2, arg3, arg4));
-        }
+        Log.v(getTag(cls), formatString(msg, arg1, arg2, arg3, arg4));
     }
 
     public static void v(String tag, String msg, Object... args) {
-        if (sHandler.isLoggable(VERBOSE)) {
-            sHandler.v(tag, formatString(msg, args));
-        }
+        Log.v(tag, formatString(msg, args));
     }
 
     public static void v(String tag, Throwable tr, String msg, Object... args) {
-        if (sHandler.isLoggable(VERBOSE)) {
-            sHandler.v(tag, formatString(msg, args), tr);
-        }
+        Log.v(tag, formatString(msg, args), tr);
     }
 
     public static void v(Class<?> cls, String msg, Object... args) {
-        if (sHandler.isLoggable(VERBOSE)) {
-            sHandler.v(getTag(cls), formatString(msg, args));
-        }
+        Log.v(getTag(cls), formatString(msg, args));
     }
 
     public static void v(Class<?> cls, Throwable tr, String msg, Object... args) {
-        if (sHandler.isLoggable(VERBOSE)) {
-            sHandler.v(getTag(cls), formatString(msg, args), tr);
-        }
+        Log.v(getTag(cls), formatString(msg, args), tr);
     }
 
     public static void v(String tag, String msg, Throwable tr) {
-        if (sHandler.isLoggable(VERBOSE)) {
-            sHandler.v(tag, msg, tr);
-        }
+        Log.v(tag, msg, tr);
     }
 
     public static void v(Class<?> cls, String msg, Throwable tr) {
-        if (sHandler.isLoggable(VERBOSE)) {
-            sHandler.v(getTag(cls), msg, tr);
-        }
+        Log.v(getTag(cls), msg, tr);
     }
 
     public static void d(String tag, String msg) {
-        if (sHandler.isLoggable(DEBUG)) {
-            sHandler.d(tag, msg);
-        }
+        Log.d(tag, msg);
     }
 
     public static void d(String tag, String msg, Object arg1) {
-        if (sHandler.isLoggable(DEBUG)) {
-            sHandler.d(tag, formatString(msg, arg1));
-        }
+        Log.d(tag, formatString(msg, arg1));
     }
 
     public static void d(String tag, String msg, Object arg1, Object arg2) {
-        if (sHandler.isLoggable(DEBUG)) {
-            sHandler.d(tag, formatString(msg, arg1, arg2));
-        }
+        Log.d(tag, formatString(msg, arg1, arg2));
     }
 
     public static void d(String tag, String msg, Object arg1, Object arg2, Object arg3) {
-        if (sHandler.isLoggable(DEBUG)) {
-            sHandler.d(tag, formatString(msg, arg1, arg2, arg3));
-        }
+
+        Log.d(tag, formatString(msg, arg1, arg2, arg3));
     }
 
     public static void d(String tag, String msg, Object arg1, Object arg2, Object arg3, Object arg4) {
-        if (sHandler.isLoggable(DEBUG)) {
-            sHandler.d(tag, formatString(msg, arg1, arg2, arg3, arg4));
-        }
+
+        Log.d(tag, formatString(msg, arg1, arg2, arg3, arg4));
     }
 
     public static void d(Class<?> cls, String msg) {
-        if (sHandler.isLoggable(DEBUG)) {
-            sHandler.d(getTag(cls), msg);
-        }
+
+        Log.d(getTag(cls), msg);
     }
 
     public static void d(Class<?> cls, String msg, Object arg1) {
-        if (sHandler.isLoggable(DEBUG)) {
-            sHandler.d(getTag(cls), formatString(msg, arg1));
-        }
+
+        Log.d(getTag(cls), formatString(msg, arg1));
     }
 
     public static void d(Class<?> cls, String msg, Object arg1, Object arg2) {
-        if (sHandler.isLoggable(DEBUG)) {
-            sHandler.d(getTag(cls), formatString(msg, arg1, arg2));
-        }
+
+        Log.d(getTag(cls), formatString(msg, arg1, arg2));
     }
 
     public static void d(Class<?> cls, String msg, Object arg1, Object arg2, Object arg3) {
-        if (sHandler.isLoggable(DEBUG)) {
-            sHandler.d(getTag(cls), formatString(msg, arg1, arg2, arg3));
-        }
+
+        Log.d(getTag(cls), formatString(msg, arg1, arg2, arg3));
     }
 
     public static void d(Class<?> cls, String msg, Object arg1, Object arg2, Object arg3,
                          Object arg4) {
-        if (sHandler.isLoggable(DEBUG)) {
-            sHandler.d(getTag(cls), formatString(msg, arg1, arg2, arg3, arg4));
-        }
+
+        Log.d(getTag(cls), formatString(msg, arg1, arg2, arg3, arg4));
     }
 
     public static void d(String tag, String msg, Object... args) {
-        if (sHandler.isLoggable(DEBUG)) {
-            d(tag, formatString(msg, args));
-        }
+
+        d(tag, formatString(msg, args));
     }
 
     public static void d(String tag, Throwable tr, String msg, Object... args) {
-        if (sHandler.isLoggable(DEBUG)) {
-            d(tag, formatString(msg, args), tr);
-        }
+
+        d(tag, formatString(msg, args), tr);
     }
 
     public static void d(Class<?> cls, String msg, Object... args) {
-        if (sHandler.isLoggable(DEBUG)) {
-            sHandler.d(getTag(cls), formatString(msg, args));
-        }
+
+        Log.d(getTag(cls), formatString(msg, args));
     }
 
     public static void d(Class<?> cls, Throwable tr, String msg, Object... args) {
-        if (sHandler.isLoggable(DEBUG)) {
-            sHandler.d(getTag(cls), formatString(msg, args), tr);
-        }
+
+        Log.d(getTag(cls), formatString(msg, args), tr);
     }
 
     public static void d(String tag, String msg, Throwable tr) {
-        if (sHandler.isLoggable(DEBUG)) {
-            sHandler.d(tag, msg, tr);
-        }
+
+        Log.d(tag, msg, tr);
     }
 
     public static void d(Class<?> cls, String msg, Throwable tr) {
-        if (sHandler.isLoggable(DEBUG)) {
-            sHandler.d(getTag(cls), msg, tr);
-        }
+
+        Log.d(getTag(cls), msg, tr);
     }
 
     public static void i(String tag, String msg) {
-        if (sHandler.isLoggable(INFO)) {
-            sHandler.i(tag, msg);
-        }
+
+        Log.i(tag, msg);
     }
 
     public static void i(String tag, String msg, Object arg1) {
-        if (sHandler.isLoggable(INFO)) {
-            sHandler.i(tag, formatString(msg, arg1));
-        }
+
+        Log.i(tag, formatString(msg, arg1));
     }
 
     public static void i(String tag, String msg, Object arg1, Object arg2) {
-        if (sHandler.isLoggable(INFO)) {
-            sHandler.i(tag, formatString(msg, arg1, arg2));
-        }
+
+        Log.i(tag, formatString(msg, arg1, arg2));
     }
 
     public static void i(String tag, String msg, Object arg1, Object arg2, Object arg3) {
-        if (sHandler.isLoggable(INFO)) {
-            sHandler.i(tag, formatString(msg, arg1, arg2, arg3));
-        }
+
+        Log.i(tag, formatString(msg, arg1, arg2, arg3));
     }
 
     public static void i(String tag, String msg, Object arg1, Object arg2, Object arg3, Object arg4) {
-        if (sHandler.isLoggable(INFO)) {
-            sHandler.i(tag, formatString(msg, arg1, arg2, arg3, arg4));
-        }
+
+        Log.i(tag, formatString(msg, arg1, arg2, arg3, arg4));
     }
 
     public static void i(Class<?> cls, String msg) {
-        if (sHandler.isLoggable(INFO)) {
-            sHandler.i(getTag(cls), msg);
-        }
+
+        Log.i(getTag(cls), msg);
     }
 
     public static void i(Class<?> cls, String msg, Object arg1) {
-        if (sHandler.isLoggable(INFO)) {
-            sHandler.i(getTag(cls), formatString(msg, arg1));
-        }
+
+        Log.i(getTag(cls), formatString(msg, arg1));
     }
 
     public static void i(Class<?> cls, String msg, Object arg1, Object arg2) {
-        if (sHandler.isLoggable(INFO)) {
-            sHandler.i(getTag(cls), formatString(msg, arg1, arg2));
-        }
+
+        Log.i(getTag(cls), formatString(msg, arg1, arg2));
     }
 
     public static void i(Class<?> cls, String msg, Object arg1, Object arg2, Object arg3) {
-        if (sHandler.isLoggable(INFO)) {
-            sHandler.i(getTag(cls), formatString(msg, arg1, arg2, arg3));
-        }
+
+        Log.i(getTag(cls), formatString(msg, arg1, arg2, arg3));
     }
 
     public static void i(Class<?> cls, String msg, Object arg1, Object arg2, Object arg3,
                          Object arg4) {
-        if (sHandler.isLoggable(INFO)) {
-            sHandler.i(getTag(cls), formatString(msg, arg1, arg2, arg3, arg4));
-        }
+
+        Log.i(getTag(cls), formatString(msg, arg1, arg2, arg3, arg4));
     }
 
     public static void i(String tag, String msg, Object... args) {
-        if (sHandler.isLoggable(INFO)) {
-            sHandler.i(tag, formatString(msg, args));
-        }
+
+        Log.i(tag, formatString(msg, args));
     }
 
     public static void i(String tag, Throwable tr, String msg, Object... args) {
-        if (sHandler.isLoggable(INFO)) {
-            sHandler.i(tag, formatString(msg, args), tr);
-        }
+
+        Log.i(tag, formatString(msg, args), tr);
     }
 
     public static void i(Class<?> cls, String msg, Object... args) {
-        if (sHandler.isLoggable(INFO)) {
-            sHandler.i(getTag(cls), formatString(msg, args));
-        }
+
+        Log.i(getTag(cls), formatString(msg, args));
     }
 
-    public static void i(Class <?> cls, Throwable tr, String msg, Object... args) {
-        if (isLoggable(INFO)) {
-            sHandler.i(getTag(cls), formatString(msg, args), tr);
-        }
+    public static void i(Class<?> cls, Throwable tr, String msg, Object... args) {
+        Log.i(getTag(cls), formatString(msg, args), tr);
     }
 
     public static void i(String tag, String msg, Throwable tr) {
-        if (sHandler.isLoggable(INFO)) {
-            sHandler.i(tag, msg, tr);
-        }
+
+        Log.i(tag, msg, tr);
     }
 
     public static void i(Class<?> cls, String msg, Throwable tr) {
-        if (sHandler.isLoggable(INFO)) {
-            sHandler.i(getTag(cls), msg, tr);
-        }
+
+        Log.i(getTag(cls), msg, tr);
     }
 
     public static void w(String tag, String msg) {
-        if (sHandler.isLoggable(WARN)) {
-            sHandler.w(tag, msg);
-        }
+        Log.w(tag, msg);
     }
 
-    public static void w(Class <?> cls, String msg) {
-        if (sHandler.isLoggable(WARN)) {
-            sHandler.w(getTag(cls), msg);
-        }
+    public static void w(Class<?> cls, String msg) {
+        Log.w(getTag(cls), msg);
     }
 
     public static void w(String tag, String msg, Object... args) {
-        if (sHandler.isLoggable(WARN)) {
-            sHandler.w(tag, formatString(msg, args));
-        }
+        Log.w(tag, formatString(msg, args));
     }
 
     public static void w(String tag, Throwable tr, String msg, Object... args) {
-        if (sHandler.isLoggable(WARN)) {
-            sHandler.w(tag, formatString(msg, args), tr);
-        }
+        Log.w(tag, formatString(msg, args), tr);
     }
 
     public static void w(Class<?> cls, String msg, Object... args) {
-        if (sHandler.isLoggable(WARN)) {
-            sHandler.w(getTag(cls), formatString(msg, args));
-        }
+        Log.w(getTag(cls), formatString(msg, args));
     }
 
     public static void w(Class<?> cls, Throwable tr, String msg, Object... args) {
-        if (isLoggable(WARN)) {
-            w(cls, formatString(msg, args), tr);
-        }
+        w(cls, formatString(msg, args), tr);
     }
 
     public static void w(String tag, String msg, Throwable tr) {
-        if (sHandler.isLoggable(WARN)) {
-            sHandler.w(tag, msg, tr);
-        }
+        Log.w(tag, msg, tr);
     }
 
-    public static void w(Class <?> cls, String msg, Throwable tr) {
-        if (sHandler.isLoggable(WARN)) {
-            sHandler.w(getTag(cls), msg, tr);
-        }
+    public static void w(Class<?> cls, String msg, Throwable tr) {
+        Log.w(getTag(cls), msg, tr);
     }
 
     public static void e(String tag, String msg) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.e(tag, msg);
-        }
+        Log.e(tag, msg);
     }
 
-    public static void e(Class <?> cls, String msg) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.e(getTag(cls), msg);
-        }
+    public static void e(Class<?> cls, String msg) {
+        Log.e(getTag(cls), msg);
     }
 
     public static void e(String tag, String msg, Object... args) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.e(tag, formatString(msg, args));
-        }
+        Log.e(tag, formatString(msg, args));
     }
 
     public static void e(String tag, Throwable tr, String msg, Object... args) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.e(tag, formatString(msg, args), tr);
-        }
+        Log.e(tag, formatString(msg, args), tr);
     }
 
     public static void e(Class<?> cls, String msg, Object... args) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.e(getTag(cls), formatString(msg, args));
-        }
+        Log.e(getTag(cls), formatString(msg, args));
     }
 
     public static void e(Class<?> cls, Throwable tr, String msg, Object... args) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.e(getTag(cls), formatString(msg, args), tr);
-        }
+        Log.e(getTag(cls), formatString(msg, args), tr);
     }
 
     public static void e(String tag, String msg, Throwable tr) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.e(tag, msg, tr);
-        }
+        Log.e(tag, msg, tr);
     }
 
-    public static void e(Class <?> cls, String msg, Throwable tr) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.e(getTag(cls), msg, tr);
-        }
+    public static void e(Class<?> cls, String msg, Throwable tr) {
+        Log.e(getTag(cls), msg, tr);
     }
 
     public static void wtf(String tag, String msg) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.e(tag, msg);
-        }
+        Log.e(tag, msg);
     }
 
-    public static void wtf(Class <?> cls, String msg) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.e(getTag(cls), msg);
-        }
+    public static void wtf(Class<?> cls, String msg) {
+        Log.e(getTag(cls), msg);
     }
 
     public static void wtf(String tag, String msg, Object... args) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.wtf(tag, formatString(msg, args));
-        }
+        Log.wtf(tag, formatString(msg, args));
     }
 
     public static void wtf(String tag, Throwable tr, String msg, Object... args) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.wtf(tag, formatString(msg, args), tr);
-        }
+        Log.wtf(tag, formatString(msg, args), tr);
     }
 
-    public static void wtf(Class <?> cls, String msg, Object... args) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.wtf(getTag(cls), formatString(msg, args));
-        }
+    public static void wtf(Class<?> cls, String msg, Object... args) {
+        Log.wtf(getTag(cls), formatString(msg, args));
     }
 
-    public static void wtf(Class <?> cls, Throwable tr, String msg, Object... args) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.wtf(getTag(cls), formatString(msg, args), tr);
-        }
+    public static void wtf(Class<?> cls, Throwable tr, String msg, Object... args) {
+        Log.wtf(getTag(cls), formatString(msg, args), tr);
     }
 
     public static void wtf(String tag, String msg, Throwable tr) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.wtf(tag, msg, tr);
-        }
+        Log.wtf(tag, msg, tr);
     }
 
-    public static void wtf(Class <?> cls, String msg, Throwable tr) {
-        if (sHandler.isLoggable(ERROR)) {
-            sHandler.wtf(getTag(cls), msg, tr);
-        }
+    public static void wtf(Class<?> cls, String msg, Throwable tr) {
+        Log.wtf(getTag(cls), msg, tr);
     }
 
     private static String formatString(String str, Object... args) {
